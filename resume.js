d2fc76d110908be9d97a630efa2f6cba96312f6d
Matheus <rbiam027@bkusl-1ewfml7h.attlocal.net>
2020-07-30 16:49:59 -0400

    this is my first commit of resume

---
+++ resume.js
@@ -0,0 +1,29 @@
+const data = {
+  name 'Alexis Matheus'
+  mainEmoji:{
+    character: '👋',
+    ariaLabel: 'wave',
+  },
+  phoneNumber: '7862024691',
+  email:'amatheus93@gmail.com',
+  githubHandle: 'andyweiss1982',
+  linkedInHandle: 'andyweiss'
+
+};
+
+document.title =data.name;
+
+const name = document .querySelector(#'name');
+
+name.textContect =data.name
+
+const emojiContainer = document.querySelector('h1 span[role="img"]');
+
+emojiContainer.textContent = data.mainEmoji.character;
+
+const phoneNumberContainer = document.querySelector('#phone-number');
+phoneNumberContainer.textContent = data.phoneNumber;
+
+const emailContainer = document.querySelector('#email');
+emailContainer = document.querySelector()
+
